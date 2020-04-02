8b58ed2b383ee887ea44fb740370e20e91064ac4
Dayana Maia <dayrmaia@gmail.com>
2020-04-02 17:40:19 -0300

    form react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import { FormPage } from './Components.js';
+
+function App() {
+  return (
+    <div className="container">
+      <FormPage />
+    </div>
+  );
+}
+
+export default App;
