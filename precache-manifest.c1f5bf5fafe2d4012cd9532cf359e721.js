ac0a7e80f7265751b71554a7bba432ae1118e5aa
Dayana Maia <dayrmaia@gmail.com>
2020-04-02 17:45:27 -0300

    Updates

---
+++ precache-manifest.c1f5bf5fafe2d4012cd9532cf359e721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e240dcf5a802892b9887c9b68f47e657",
+    "url": "/tarefa-bootcamp-react-form/index.html"
+  },
+  {
+    "revision": "518baeba5b9e2fbdbbb8",
+    "url": "/tarefa-bootcamp-react-form/static/css/main.525d7c59.chunk.css"
+  },
+  {
+    "revision": "6facdb9110b2f9dc46fa",
+    "url": "/tarefa-bootcamp-react-form/static/js/2.fd124eda.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tarefa-bootcamp-react-form/static/js/2.fd124eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518baeba5b9e2fbdbbb8",
+    "url": "/tarefa-bootcamp-react-form/static/js/main.0dccc75b.chunk.js"
+  },
+  {
+    "revision": "35306d86ec4aaf600bf0",
+    "url": "/tarefa-bootcamp-react-form/static/js/runtime-main.84782d5e.js"
+  }
+]);
